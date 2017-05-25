2f37da06b5a69fc7f47aebdf7419682650460d6a
Daniel Bernal <dbernal@xumak.com>
2017-05-25 17:24:02 -0500

    complete PageMotionTitle

--- Prototypes/xumakSite2017/source/sections/pageMotionTitle/libs/scripts/component.js
+++ Prototypes/xumakSite2017/source/sections/pageMotionTitle/libs/scripts/component.js
@@ -6,28 +6,31 @@ Vue.component( 'page-motion-title', {
     ],
     template: '<div><slot></slot></div>',
     mounted: function() {
-
       var tlXumakText = new TimelineMax (),
+      mediaquery = window.matchMedia( "(min-width: 1025px)" ),
       tl2 = new TimelineLite(),
-      mainText = $(this.$el).find( '.pagaMotionTitle-component' ),
-      distancia = mainText.offsetWidth * -1;
-      console.log( distancia );
-      if ( document.body.scrollTop === 0 ) {
-        tlXumakText.from( mainText, 1.5, { left: '100vw' }, '0' );
-        tlXumakText.play( );
+      mainText = $( this.$el ).find( '.pagaMotionTitle-component' ),
+      distancia = mainText[ 0 ].offsetWidth * -1;
+
+      if ( mediaquery.matches ) {
+        if ( document.body.scrollTop === 0 ) {
+          tlXumakText.from( mainText, 1.5, { left: '100vw' }, '0' );
+          tlXumakText.play( );
+        }
+
+        tl2.to( mainText, 1, { left: distancia + 'px' } );
+        tl2.play();
+
+        new ScrollMagic.Scene({
+          duration: '200%',
+        } )
+        .setTween( tl2 )
+        .triggerHook( 0 )
+        .triggerElement( '#xmkApp' )
+        .addIndicators( {name: 'text '} )
+        .addTo( this.controller );
       }
 
-      tl2.to( mainText, 1, { left: distancia + 'px' }, '0' );
-      tl2.play();
-
-      new ScrollMagic.Scene({
-        duration: '200%',
-      } )
-      .setTween( tl2 )
-      .triggerHook( 0 )
-      .triggerElement( '#xmkApp' )
-      .addIndicators( {name: 'text '} )
-      .addTo( this.controller );
 
     },
   }
