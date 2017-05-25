fd043aea0a66c8d23db733026b1fba532c3d4bc5
Marco Cali <mcali@minimaceth-13.xumak.gt>
2017-05-25 08:40:54 -0600

    [XCOM-160] Get Path From Resources Context Processor: added some content and validations to get properties for unit test.

--- JahiaFiles/components/src/test/java/com/xumak/contextprocessors/GetPathFromResourcesContextProcessorTest.java
+++ JahiaFiles/components/src/test/java/com/xumak/contextprocessors/GetPathFromResourcesContextProcessorTest.java
@@ -1,17 +1,22 @@
 package com.xumak.contextprocessors;
 
+import javax.jcr.PropertyType;
 import com.xumak.base.configuration.MockLayerXConfiguration;
 import com.xumak.base.configuration.MockLayerXConfigurationProvider;
 import com.xumak.base.templatingsupport.BaseTest;
-import org.jahia.services.content.JCRNodeIteratorWrapper;
 import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRPropertyWrapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,13 +40,12 @@ import static org.junit.Assert.*;
 @RunWith(MockitoJUnitRunner.class)
 public class GetPathFromResourcesContextProcessorTest {
 
-    private JCRNodeWrapper jcrNodeWrapper;
-    private JCRNodeIteratorWrapper jcrNodeIteratorWrapper;
-
+    private JCRPropertyWrapper propertyWrapper;
     private static String IMAGE1 = "image";
     private static String IMAGE2 = "imageTwo";
     private static String PAGE1 = "pageOne";
     private static String PAGE2 = "pageTwo";
+    private static String resourceAsUUID = "myResourceUUID";
 
     @Mock
     public MockLayerXConfigurationProvider configurationProvider;
@@ -50,7 +54,7 @@ public class GetPathFromResourcesContextProcessorTest {
     public MockLayerXConfiguration configuration;
 
     @InjectMocks
-    private ItemsContainerContextProcessor itemsContainerCP = new ItemsContainerContextProcessor();
+    private GetPathFromResourcesContextProcessor getPathFromResourcesCP = new GetPathFromResourcesContextProcessor();
 
     private BaseTest baseTest;
 
@@ -58,8 +62,7 @@ public class GetPathFromResourcesContextProcessorTest {
     public void setUp() throws Exception {
 
         baseTest = new BaseTest();
-        jcrNodeWrapper = Mockito.mock(JCRNodeWrapper.class);
-        jcrNodeIteratorWrapper = Mockito.mock(JCRNodeIteratorWrapper.class);
+        propertyWrapper = Mockito.mock(JCRPropertyWrapper.class);
 
         List<String> list = new ArrayList<String>();
         list.add(IMAGE1);
@@ -68,13 +71,27 @@ public class GetPathFromResourcesContextProcessorTest {
         list.add(PAGE2);
         baseTest.initializeConfiguration(configurationProvider, configuration);
         baseTest.config.put(PATH_FROM_RESOURCE_PROPERTIES_LIST, list);
+        baseTest.jcrNodeWrapper.setProperty(IMAGE1, resourceAsUUID);
     }
 
     @Test
-    public void testConfigNodePropertiesIsNotNull() throws Exception {
-        itemsContainerCP.process(baseTest.executionContext, baseTest.contentModel);
-        final Object nodeItemPropertiesObject = baseTest.contentModel.get(CONFIG_PROPERTIES_KEY + DOT + PATH_FROM_RESOURCE_PROPERTIES_LIST);
+    public void testNodePropertiesType() throws Exception {
+        Answer<JCRPropertyWrapper> answer = new Answer<JCRPropertyWrapper>() {
+            @Override
+            public JCRPropertyWrapper answer(InvocationOnMock invocationOnMock) throws Throwable {
+                return propertyWrapper;
+            }
+        };
+        getPathFromResourcesCP.process(baseTest.executionContext, baseTest.contentModel);
+
+        final List<String> nodeItemPropertiesObject = (List<String>) baseTest.contentModel.get(CONFIG_PROPERTIES_KEY
+                + DOT + PATH_FROM_RESOURCE_PROPERTIES_LIST);
         assertNotNull(nodeItemPropertiesObject);
+
+        boolean hasProperty = true;
+        when(baseTest.jcrNodeWrapper.setProperty(IMAGE1, resourceAsUUID)).thenAnswer(answer);
+        when(baseTest.jcrNodeWrapper.hasProperty(IMAGE1)).thenReturn(hasProperty);
+
     }
 
 }
