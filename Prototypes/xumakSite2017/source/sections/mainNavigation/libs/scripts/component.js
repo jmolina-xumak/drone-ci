7cfc683278c37738e28fbf17ad7e51018e73de18
alejandro navarro <anavarro@xumak.com>
2017-05-26 16:50:11 -0500

    Final changes for the main navigation task xcom-114

--- Prototypes/xumakSite2017/source/sections/mainNavigation/libs/scripts/component.js
+++ Prototypes/xumakSite2017/source/sections/mainNavigation/libs/scripts/component.js
@@ -18,32 +18,20 @@ Vue.component('mainNavigation', function mainNavCreator(resolve, reject) {
                     return el;
                 });
                 vm.$set(this.menu, 'isVisible', false);
+                vm.$set(this.menu, 'isSlided', false);
             },
             methods: {
-                toggleSubMenu: function(item){
-                    if(item.hasChildren && $(window).width() < 1224){
-                        let isExpanded = !item.isExpanded;
-
-                        this.menu.pages = this.menu.pages.map(function(el){
-                            el.isExpanded = false;
-                            return el;
-                        });
-                        if(isExpanded === true ){
-                            item.isExpanded = isExpanded;
-                        }
-                    }else {
-                        return false;
-                    }
-                },
                 toggleMenu: function() {
                     this.menu.isVisible = !this.menu.isVisible;
-                    
+                    this.menu.isSlided = !this.menu.isSlided;
+                    this.mapReset();
+                    $('.MainNavigation-subList').attr('style','')
                     var self = this;
                     if ( this.menu.isVisible ) {
                         $('<div class="MainNavigation-overlay"/>').appendTo('body').animate({
                             opacity: .8
                         }, 500).on('click', function(){
-                            self.toggleMenu();
+                            self.removeOverlay();
                         })
                       
                     } else {
@@ -54,49 +42,86 @@ Vue.component('mainNavigation', function mainNavCreator(resolve, reject) {
                         });
                     }
                 },
-                mouseIn: function() {
-                    if($(window).width() > 1224) {
-                        $('.MainNavigation-component').addClass('isSlidedIn').removeClass('isSlidedOut');
+                mouseEnter: function(item, el){
+                    //If the element is expanded close himself
+                    if(item.isExpanded) {
+                        item.isExpanded = !item.isExpanded;
+                        $('.MainNavigation-subList.isExpanded').css({
+                            'animation': 'out .5s ease forwards'
+                        });
+                        
+                        return false
+                    }
+                    var self = this;
+
+                    this.mapReset();
+
+                    if(item.hasChildren ) {
+                        $('.MainNavigation-arrow.isActive').removeClass('isActive');
+                        item.isExpanded = true;
+                        this.menu.isVisible = true;
+
+                        var listItem = $(el.target).parents('.MainNavigation-item');
+                        //if some of the link in the main nav has the active state then remove the stata
+                        $('.MainNavigation-link').removeClass('isActive');
+
+                        //if some submenu is visible, collapse it
+                        $('.MainNavigation-subList.isExpanded').css({
+                            'animation': 'out .5s ease forwards'
+                        });
+
+                        //Set the active class to clicked link
+                        $(el.target).parents('.MainNavigation-item').find('.MainNavigation-link').addClass('isActive');
+
+                        //Show the main menu background if dont have it yet
+                        this.menu.isSlided = true;
+
+                        //Show the submenu of the item clicked
+                        $(listItem[0]).find('.MainNavigation-subList').addClass('isExpanded').css({
+                            'animation':'in .5s .3s ease forwards'
+                        });
+                        
+                        //show the overlay if dont exist
                         if($('.MainNavigation-overlay').length > 0){
                                 return false;
                         } else {
                             $('<div class="MainNavigation-overlay"/>').appendTo('body').animate({
                                 opacity: .8
-                            }, 500);
+                            }, 500).on('click', function(){
+                                self.removeOverlay();
+                            });
                         }
-                    }
-                },
-                mouseOut: function(){
-                    if($(window).width() > 1224) { 
-                        $('.MainNavigation-component').addClass('isSlidedOut').removeClass('isSlidedIn');    
-                        
-                        $('.MainNavigation-overlay').animate({
-                            opacity: 0
-                        }, 500, function() {
-                            $(this).remove();
-                        });
-                    }
-                },
-                mouseEnter: function(item, el){
-                    if(item.hasChildren && $(window).width() > 1224) {
-                        var listItem = $(el.target).parents('.MainNavigation-item');
-                        $(el.target).parents('.MainNavigation-item').find('.MainNavigation-link').addClass('isActive');
-                        $(listItem[0]).find('.MainNavigation-subList').addClass('isExpanded').css({
-                            'animation':'in .5s .3s ease forwards'
-                        });;
                     } else {
                         return false;
                     }
                 },
-                mouseLeave: function(item, el){
-                    if(item.hasChildren && $(window).width() > 1224) {  
-                        $(el.target).find('.MainNavigation-link').removeClass('isActive');
-                        $('.MainNavigation-subList.isExpanded').css({
-                            'animation':'out .5s ease forwards'
-                        });
-                    } else {
-                        return false;
-                    }
+                removeOverlay: function() {
+
+                    this.mapReset();
+                    //collapse sub menu and remove the main menu background
+                    $('.MainNavigation-arrow.isActive').removeClass('isActive');
+                    $('.MainNavigation-component.isActive').removeClass('isActive');
+                    $('.MainNavigation-subList.isExpanded').css({
+                        'animation':'out .5s ease forwards'
+                    }).removeClass('isExpanded');
+
+                    //Delay to wait until sub menu collapse complety
+                    setTimeout(function(){
+                        this.menu.isSlided = false;
+                        this.menu.isVisible = false;
+
+                    }.bind(this), 500);
+
+                    $('.MainNavigation-link').removeClass('isActive');
+                    
+                    //remove the overlay
+                    $('.MainNavigation-overlay').remove();
+                },
+                mapReset: function() {
+                    this.menu.pages = this.menu.pages.map(function(el) {
+                        el.isExpanded = false;
+                        return el;
+                    });
                 }
             }
         })
